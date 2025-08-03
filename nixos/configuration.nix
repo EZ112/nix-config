a9af38fdee8518a4c7d5a5c6da998d6b93890dde
EZ112 <izzidz@gmail.com>
2025-08-03 22:13:24 +0700

    add nix config

---
+++ nixos/configuration.nix
@@ -0,0 +1,76 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+let
+  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz;
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+      (import "${home-manager}/nixos")
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Hyprland
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+  # hint electron app to use wayland
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet -r -t -c Hyprland";
+      };
+    };
+  };
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
+  networking.hostName = "ez"; # Define your hostname.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "Asia/Jakarta";
+
+  users.users.ez = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" ]; 
+  };
+
+  home-manager.users.ez = /home/ez/dotfiles/home.nix;
+  home-manager.backupFileExtension = "backup";
+
+  programs.starship.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+     brightnessctl
+     neovim 
+     wget
+     neofetch
+     keychain
+     starship
+  ];
+
+  # Font
+  fonts.packages = with pkgs; [
+     inconsolata-nerdfont
+  ];
+
+  system.stateVersion = "25.05"; # Do not Change this value
+}
+
