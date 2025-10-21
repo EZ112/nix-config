b1c553a23e864617bd3307c2672d6ac4cb920225
Izzi <izzidz@gmail.com>
2025-10-21 22:02:04 +0700

    feat: update lsp diagnostic config

--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -4,10 +4,9 @@ vim.lsp.enable({
 
 local diagnostic = vim.diagnostic
 diagnostic.config({
-  virtual_lines = true,
+  virtual_text = true,
   underline = true,
   update_in_insert = false,
-  severity_sort = true,
   float = {
     border = 'rounded',
     source = true,
