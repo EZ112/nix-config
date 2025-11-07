5e6221c62aca5c50bcf798a8878ac7d580c9f647
Izzi <izzidz@gmail.com>
2025-11-07 14:46:33 +0700

    feat: allow unfree

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -36,6 +36,7 @@ in
     ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
 
   system.stateVersion = "25.05"; # Do not Change this value
 }
