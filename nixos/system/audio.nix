7d42d5d6704f95d9439e219d038b35ad336f6745
Izzi <izzidz@gmail.com>
2025-11-08 11:51:42 +0700

    chore: move pulse audio to services

--- nixos/system/audio.nix
+++ nixos/system/audio.nix
@@ -1,15 +1,15 @@
-
 { config, lib, pkgs, ... }:
 
 {
-  hardware.pulseaudio.enable = false;
-
   security.rtkit.enable = true;
 
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    pulse.enable = true;
+  services = {
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      pulse.enable = true;
+    };
   };
 }
 
