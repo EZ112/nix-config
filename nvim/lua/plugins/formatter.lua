4007e836be2ff54ce489108c0040a907fd04f515
Izzi <izzidz@gmail.com>
2025-11-11 13:06:12 +0700

    feat: move formatter to conform

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -1,10 +1,27 @@
 return {
   'stevearc/conform.nvim',
   config = function()
+    local prettier_ft = {
+      'javascript',
+      'javascriptreact',
+      'typescript',
+      'typescriptreact',
+      'json',
+      'jsonc',
+      'toml',
+      'yaml',
+      'css',
+      'html',
+      'markdown',
+    }
+    local formatters_by_ft = {}
+    for _, ft in ipairs(prettier_ft) do
+      formatters_by_ft[ft] = { 'prettierd' }
+    end
+    formatters_by_ft['lua'] = { 'stylua' }
+
     require('conform').setup({
-      formatters_by_ft = {
-        lua = { 'stylua' },
-      },
+      formatters_by_ft = formatters_by_ft,
     })
     vim.api.nvim_create_autocmd('BufWritePre', {
       pattern = '*',
