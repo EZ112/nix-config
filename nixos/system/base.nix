0700d25dfc0d12997bb4c7b7eddbc0e423ffe727
Izzi <izzidz@gmail.com>
2026-03-27 21:05:05 +0700

    feat: switch bootloader to grub for auto detect windows

--- nixos/system/base.nix
+++ nixos/system/base.nix
@@ -6,8 +6,30 @@
 }:
 
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+
+      extraEntries = ''
+        search --no-floppy --file --set=win_root /Windows/Boot/EFI/bootmgfw.efi
+
+        if [ -n "$win_root" ]; then
+          menuentry "Windows" {
+            insmod part_gpt
+            insmod ntfs
+
+            set root=($win_root)
+            chainloader /Windows/Boot/EFI/bootmgfw.efi
+          }
+        fi
+      '';
+    };
+    timeout = 5;
+
+    efi.canTouchEfiVariables = true;
+  };
 
   time.timeZone = "Asia/Jakarta";
 }
